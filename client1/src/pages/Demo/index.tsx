b50bce63ecdf2c4bfb1bac7bd816e7f6f6c45b6b
RubbishZhYic <38401412+ZhongYic00@users.noreply.github.com>
2023-10-05 12:18:00 +0000

    init: create using antd-pro template

---
+++ client1/src/pages/Demo/index.tsx
@@ -0,0 +1,28 @@
+import { PageContainer } from '@ant-design/pro-components';
+import { useModel } from '@umijs/max';
+import { Card, theme } from 'antd';
+import React from 'react';
+
+const Demo: React.FC = () => {
+  const { token } = theme.useToken();
+  const { initialState } = useModel('@@initialState');
+  return (
+    <PageContainer>
+      <Card
+        style={{
+          borderRadius: 8,
+        }}
+        bodyStyle={{
+          backgroundImage:
+            initialState?.settings?.navTheme === 'realDark'
+              ? 'background-image: linear-gradient(75deg, #1A1B1F 0%, #191C1F 100%)'
+              : 'background-image: linear-gradient(75deg, #FBFDFF 0%, #F5F7FF 100%)',
+        }}
+      >
+        
+      </Card>
+    </PageContainer>
+  );
+};
+
+export default Demo;
