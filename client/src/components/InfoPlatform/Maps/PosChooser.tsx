5d1458d848ad05cb9c4461d90a399ff2711cefe1
zhongyic00 <zhong200234@outlook.com>
2023-11-10 15:50:38 +0800

    feat: map show around component; refactor: poschooser to a component;

---
+++ client/src/components/InfoPlatform/Maps/PosChooser.tsx
@@ -0,0 +1,59 @@
+import { PlusOutlined } from '@ant-design/icons';
+import { Map, Marker, toLnglat } from '@pansy/react-amap';
+import { message } from 'antd';
+import { MapProps } from '@pansy/react-amap/es/map';
+
+import React, { useState } from 'react';
+// 武汉大学计算机学院的经纬度
+export const WHUCSCoords = {
+  lng: 114.357365,
+  lat: 30.538707
+};
+export const WHUCoords = {
+  lng: 114.365519,
+  lat: 30.5381
+}
+export type Pos = typeof WHUCSCoords
+export const PosChooser:React.FC<{setPos:(pos:any)=>void,initialPos:Pos}> = ({setPos,initialPos}) => {
+
+  const mapEvents: MapProps['events'] = {
+    click: (e) => { message.info(`点击的坐标为${e.lnglat}`) 
+    setCenterCoords({lng:e.lnglat.lng,lat:e.lnglat.lat})
+  }
+  }
+
+  const [centerCoords, setCenterCoords] = useState(initialPos);
+
+  const centerCrossStyle = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: '20px',
+    height: '20px',
+  };
+
+  const handleConfirmClick = () => {
+    // 点击确认按钮时，显示地图中心点的经纬度
+    // alert(`地图中心点的经度：${centerCoords.longitude}, 纬度：${centerCoords.latitude}`);
+    setPos?.(centerCoords)
+  };
+
+  return (
+    <div style={{ height: 500, position: 'relative' }}>
+      <Map
+        zoom={18}
+        center={centerCoords}
+        events={mapEvents}
+      
+      >
+        <div>
+          <Marker position={centerCoords} />
+        </div>
+      </Map>
+      <PlusOutlined style={centerCrossStyle} />
+      <button onClick={handleConfirmClick} style={{ position: 'absolute', bottom: '20px', left: '50%', transform: 'translateX(-50%)' }}>确认</button>
+
+    </div>
+  );
+};
--- client/src/pages/Maps/index.tsx
+++ client/src/pages/Maps/index.tsx
@@ -1,54 +1,23 @@
-import { PlusOutlined } from '@ant-design/icons';
-import { Map, Marker } from '@pansy/react-amap';
-import { message } from 'antd';
-import { MapProps } from '@pansy/react-amap/es/map';
+import { Around } from "@/components/InfoPlatform/Maps/Around";
+import { PosChooser, WHUCSCoords, WHUCoords } from "@/components/InfoPlatform/Maps/PosChooser";
+import { Col } from "antd";
 
-import React, { useState } from 'react';
+export default ()=>{
 
-export default () => {
-  // 武汉大学计算机学院的经纬度
-  const wuhanUniversityCoords = {
-    longitude: 114.357365,
-    latitude: 30.538707
-  };
+const randomLnglat = () => [
+  WHUCoords.lng + Math.random() * 0.01-0.005,
+  WHUCoords.lat + Math.random() * 0.01-0.005
+] as AMap.MarkerCluster.DataOptions['lnglat'];
 
-  const mapEvents: MapProps['events'] = {
-    click: (e) => { message.info(`点击的坐标为${e.lnglat}`) 
-    setCenterCoords({longitude:e.lnglat.lng,latitude:e.lnglat.lat})
-  }
-  }
+const randomMarker = (len = 10) => (
+  Array(len).fill(true).map((item, index) => ({
+    lnglat: randomLnglat(),
+    extData: index+'s'
+  }))
+);
 
-  const [centerCoords, setCenterCoords] = useState(wuhanUniversityCoords);
-
-  const centerCrossStyle = {
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    width: '20px',
-    height: '20px',
-  };
-
-  const handleConfirmClick = () => {
-    // 点击确认按钮时，显示地图中心点的经纬度
-    alert(`地图中心点的经度：${centerCoords.longitude}, 纬度：${centerCoords.latitude}`);
-  };
-
-  return (
-    <div style={{ height: 500, position: 'relative' }}>
-      <Map
-        zoom={15}
-        center={wuhanUniversityCoords}
-        events={mapEvents}
-      
-      >
-        <div>
-          <Marker position={centerCoords} />
-        </div>
-      </Map>
-      <PlusOutlined style={centerCrossStyle} />
-      <button onClick={handleConfirmClick} style={{ position: 'absolute', bottom: '20px', left: '50%', transform: 'translateX(-50%)' }}>确认</button>
-
-    </div>
-  );
+  return <Col>
+    <PosChooser setPos={()=>null} initialPos={WHUCSCoords}/>
+    <Around objects={randomMarker(100)} />
+  </Col>
 };
